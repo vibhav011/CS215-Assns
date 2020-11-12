c4e6dbdd8054bc09baaf79de348fb19ce7e1ec3b
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-11-12 08:22:37 +0530

    Initial commit

---
+++ Assignment 4/code/q2.m
@@ -0,0 +1,57 @@
+clear;
+close all;
+
+rng(190050128);
+
+mu = [1; 2];
+C = [1.6250, -1.9486; -1.9486, 3.8750];          % Given covariance matrix
+[U, D2] = eig(C);
+A = U*sqrt(D2)*U';
+
+samples = [10 100 1000 10000 100000];
+mean_error = zeros(100, 5);         % For boxplot of error in ML mean
+cov_error = zeros(100, 5);          % For boxplot of error in ML cov
+
+for i = 1:5
+    N = samples(i);
+    
+    % Part (b) and (c)
+    
+    for trial = 1:100
+        W = randn([2 N]);
+        X = A*W + mu;                  % Generated sample with required mean and covariance matrix
+        
+        mu_ = sum(X, 2)/N;              % ML estimate of mean
+        mu_error = sqrt((mu - mu_)' * (mu - mu_) / (mu' * mu));
+        mean_error(trial, i) = mu_error;
+        
+        C_ = (X - mu_) * (X - mu_)' / N;              % ML estimate of covariance matrix
+        C_error = sqrt(sum((C - C_) .^2, 'all') / sum(C .^2, 'all'));
+        cov_error(trial, i) = C_error;
+    end
+    
+    % Part (d)
+    [U_, D_] = eig(C_);
+    l = diag(sqrt(D_));                 % 2x1 vector containing eigenvalues
+    
+    figure;
+    scatter(X(1, :), X(2, :));
+    axis equal;
+    hold on;
+    X1 = [mu_(1) mu_(1)];
+    Y1 = [mu_(2) mu_(2)];
+    X2 = X1 + [l(1)*U_(1, 1) l(2)*U_(1, 2)];
+    Y2 = Y1 + [l(1)*U_(2, 1) l(2)*U_(2, 2)];
+    plot([X1; X2], [Y1; Y2], 'r');
+    hold off;
+    title(['Scatter plot and principal modes when N = ', num2str(N)]);
+end
+
+figure;
+boxplot(mean_error);
+title('Error between true mean and ML estimate of mean');
+xlabel('log_{10}(N)');
+figure;
+boxplot(cov_error);
+title('Error between true cov and ML estimate of cov matrix');
+xlabel('log_{10}(N)');
---
+++ Assignment 4/code/q4.m
@@ -0,0 +1,37 @@
+clear;
+close all;
+S = load('data/mnist.mat');
+
+digits = double(S.digits_train);
+digits = reshape(digits, 784, size(digits, 3));         % digits is now a 784x60000 matrix
+labels = transpose(S.labels_train);                     % labels is a 1x60000 row
+
+Means = zeros(784, 10);
+Covs = zeros(784, 784, 10);
+Eigvecs = zeros(784, 10);
+Eigvals = zeros(10);
+
+for i = 0:9
+    indices = find(labels == i);                        % extracting indices of current digit
+    current_digit = digits(:, indices);
+    N = size(current_digit, 2);                         % Number of samples
+    mean = sum(current_digit, 2)/N;                     % 784x1 vector
+    current_digit = current_digit - mean;
+    C = current_digit * current_digit' / max(1, N-1);   % 784x784 covariance matrix
+    
+    [U, S] = eig(C);
+    [D, ind] = sort(diag(S), 'descend');
+    
+    Means(:, i+1) = mean;
+    Covs(:, :, i+1) = C;
+    Eigvecs(:, i+1) = U(:, ind(1));
+    Eigvals(i+1) = D(1);
+    
+    plot(D);
+    hold on; 
+end
+hold off;
+xlim([0 50]);                       % Truncating rest of the values
+legend('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+title('Plot of 784 eigenvalues for each digit');
+ylabel('Eigenvalue');
