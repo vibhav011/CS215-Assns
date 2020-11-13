826f5b42b2e5c45b69391c27e32ccc8df46543ea
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-11-13 05:54:35 +0530

    Added comments in q1

--- Assignment 4/code/q1.m
+++ Assignment 4/code/q1.m
@@ -3,19 +3,19 @@ close all;
 
 rng(190050125);
 
-N = 10 .^ (1:8);
+N = 10 .^ (1:8);                            % Array for differnt values of N
 for i=1:length(N)
-    batch_size = min(10^5, N(i));
+    batch_size = min(10^5, N(i));           % Restricting batch size to 10^5
     num_batches = floor(N(i)/batch_size);
     total = 0;
     
     for j = 1:num_batches
-        sample1 = rand(batch_size,1);
-        sample2 = rand(batch_size,1);
+        sample1 = rand(batch_size,1);         % Generating X1
+        sample2 = rand(batch_size,1);         % Generating X2
         total = total + sum((sample1.^2+sample2.^2)<=1);
     end
     
-    rem = mod(N(i), batch_size);
+    rem = mod(N(i), batch_size);            % Remaining points (if N is not a power of 10)
     
     if rem > 0
         sample1 = rand(rem, 1);
