98dea28e51e7af35d8772047d6d962c952a92166
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2020-11-25 21:33:43 +0530

    updated

--- Assignment 5/code/q2.m
+++ Assignment 5/code/q2.m
@@ -1,3 +1,8 @@
+
+
+
+
+
 clear;
 clc;
 close all;
@@ -5,23 +10,27 @@ M = 100;
 lambda = 5;
 alpha = 5.5;
 beta = 1;
-for N = [5 10 20 40 60 80 100 500 1000 10000]
-    sample = -0.2*log(rand(N,M));
+A = zeros(100,10);
+B = zeros(100,10);
+N = [5 10 20 40 60 80 100 500 1000 10000];
+for i=1:10
+    sample = -0.2*log(rand(N(i),M));
     sample_sum = sum(sample,1);
-    lambda_ML = N./sample_sum;
-    lambda_MAP = (N+alpha-1)./(sample_sum+beta);
-    A = transpose(abs(lambda_ML-lambda)/lambda);
-    B = transpose(abs(lambda_MAP-lambda)/lambda);
-    group = [ones(size(A));
-         2 * ones(size(A))];
-    figure
-    boxplot([A; B],group)
-    title(N+" sample points");
-    set(gca,'XTickLabel',{'lambda_ML','lambda_MAP'})
+    lambda_ML = N(i)./sample_sum;
+    lambda_MAP = (N(i)+alpha)./(sample_sum+beta);
+    A(:,i) = transpose(abs(lambda_ML-lambda)/lambda);
+    B(:,i) = transpose(abs(lambda_MAP-lambda)/lambda);   
 end
+figure;
+boxplot(B,'Color',"r");
+hold on;
+boxplot(A,'Color',"b");
+ylabel('Relative errors for estimates of lambda');
+xlabel('N')
+set(gca,'XTickLabel',{'5','10','20','40','60','80','100','500',' 1000 ','   10000'});
 
 
-
+hold off;
 
 
 
