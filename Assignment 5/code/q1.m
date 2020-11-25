a2f1a02681a2b59d1fecd73819a6e5a75c0b7072
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2020-11-25 21:34:16 +0530

    updated

--- Assignment 5/code/q1.m
+++ Assignment 5/code/q1.m
@@ -3,25 +3,44 @@ clc;
 close all;
 M = 100;
 mu = 10.5;
-for N = [5 10 20 40 60 80 100 500 1000 10000]
-    sample = 4*randn(N,M)+10.5;
-    sample_mean = sum(sample,1)/N;
+N = [5 10 20 40 60 80 100 500 1000 10000];
+A = zeros(100,10);
+B = zeros(100,10);
+C = zeros(100,10);
+for i=1:10    
+    sample = 4*randn(N(i),M)+10.5;
+    sample_mean = sum(sample,1)/N(i);
     mu_ML = sample_mean;
-    mu_MAP_gaussian_prior = (sample_mean+16*10.5)/17;
+    mu_MAP_gaussian_prior = (sample_mean*1+10.5*16/N(i))/(1+16/N(i));
     mu_MAP_uniform_prior = sample_mean;
-    A = transpose(abs(mu_ML-mu)/mu);
-    B = transpose(abs(mu_MAP_gaussian_prior-mu)/mu);
-    C = transpose(abs(mu_MAP_uniform_prior-mu)/mu);
-    group = [    ones(size(A));
-         2 * ones(size(A));
-         3 * ones(size(A))];
-    figure
-    boxplot([A; B; C],group)
-    title(N+" sample points");
-    set(gca,'XTickLabel',{'mu_ML','mu_MAP1','mu_MAP2'})
+    mu_MAP_uniform_prior( mu_MAP_uniform_prior>11.5) = 11.5;
+    mu_MAP_uniform_prior( mu_MAP_uniform_prior<9.5) = 9.5;
+    A(:,i) = transpose(abs(mu_ML-mu)/mu);
+    B(:,i) = transpose(abs(mu_MAP_gaussian_prior-mu)/mu);
+    C(:,i) = transpose(abs(mu_MAP_uniform_prior-mu)/mu);      
 end
 
+figure;
+boxplot(B,"Color","b");
 
+%h = findobj(gca,'Tag','Box');
+%for j=1:length(h)
+%    patch(get(h(j),'XData'),get(h(j),'YData'));
+%end
+hold on;
+boxplot(C,"Color","g");
+
+%h2 = findobj(gca,'Tag','Box');
+%for j=1:length(h2)
+%    patch(get(h2(j),'XData'),get(h2(j),'YData'));
+%end
+hold on;
+boxplot(A,"Color","r");
+ylabel('Relative errors for estimates of lambda');
+xlabel('N')
+set(gca,'XTickLabel',{'5','10','20','40','60','80','100','500',' 1000 ','   10000'});
+
+hold off;
 
 
 
