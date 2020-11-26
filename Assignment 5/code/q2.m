53b38a3caca839cb2fe6cc620cbf64d2e6329c0b
Tulip Pandey <65901116+Tulip16@users.noreply.github.com>
2020-11-26 11:11:33 +0530

    comments added

--- Assignment 5/code/q2.m
+++ Assignment 5/code/q2.m
@@ -9,19 +9,21 @@ close all;
 rng(190050125);
 
 M = 100;
-lambda = 5;
+lambda = 5;  % true value 
 alpha = 5.5;
 beta = 1;
 A = zeros(100,10);
 B = zeros(100,10);
 N = [5 10 20 40 60 80 100 500 1000 10000];
 for i=1:10
-    sample = -0.2*log(rand(N(i),M));
+    sample = -0.2*log(rand(N(i),M)); % generating transformed variable y 
     sample_sum = sum(sample,1);
+   % using estimate expressions calculated in the report 
     lambda_ML = N(i)./sample_sum;
     lambda_MAP = (N(i)+alpha)./(sample_sum+beta);
-    A(:,i) = transpose(abs(lambda_ML-lambda)/lambda);
-    B(:,i) = transpose(abs(lambda_MAP-lambda)/lambda);   
+    
+    A(:,i) = transpose(abs(lambda_ML-lambda)/lambda); % stores the relative errors for ML estimate
+    B(:,i) = transpose(abs(lambda_MAP-lambda)/lambda);   %  stores the relative errors for Mean Aposteriori estimate
 end
 
 
