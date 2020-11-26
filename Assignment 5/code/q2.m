910bff036ef27624088fd9098bbbbbb8a1c31a8a
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-11-26 11:29:26 +0530

    Added q2 results

--- Assignment 5/code/q2.m
+++ Assignment 5/code/q2.m
@@ -1,24 +1,21 @@
-
-
-
-
-
 clear;
-clc;
 close all;
 rng(190050125);
 
 M = 100;
-lambda = 5;  % true value 
+lambda = 5;                             % true value 
 alpha = 5.5;
 beta = 1;
-A = zeros(100,10);
-B = zeros(100,10);
+
+A = zeros(100,10);              % To store ML estimate erros
+B = zeros(100,10);              % To store MeanPosterior estimate errors
 N = [5 10 20 40 60 80 100 500 1000 10000];
+
 for i=1:10
-    sample = -0.2*log(rand(N(i),M)); % generating transformed variable y 
+    sample = -(1/lambda) * log(rand(N(i),M));            % generating transformed variable y 
     sample_sum = sum(sample,1);
-   % using estimate expressions calculated in the report 
+    
+    % using estimate expressions calculated in the report 
     lambda_ML = N(i)./sample_sum;
     lambda_MAP = (N(i)+alpha)./(sample_sum+beta);
     
@@ -39,10 +36,3 @@ boxplot(B, 'Labels', {'5','10','20','40','60','80','100','500','1000','10000'});
 xlabel("N");
 ylabel("Relative error for Mean Aposteriori Estimate");
 title("Mean Aposteriori Estimate");
-
-
-
-
-
-
-
